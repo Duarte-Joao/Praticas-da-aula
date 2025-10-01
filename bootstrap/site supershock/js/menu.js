7b00c1d6196db918fba2df64a408a8393631f5a5
João <joaoeddgamer@gmail.com>
2025-10-01 15:15:37 -0300

    site supershock finalizado

--- bootstrap/site supershock/js/menu.js
+++ bootstrap/site supershock/js/menu.js
@@ -3,4 +3,10 @@ function mostrarMenu(){
     const iconMenu = document.getElementById('img-menu');
 
     menuLateral.classList.toggle('ativa');
+
+    if(menuLateral.classList.contains('ativa')){
+        iconMenu.src = "img/icon-close-menu.png"
+    }else{
+        iconMenu.src = "img\icon-hamburger-menu.png"
+    }
 }
