6d65e354611c8c5f052d0dff2beb3c1db612ba87
Joao <joaoeddgamer@gmail.com>
2025-10-23 14:30:48 -0300

    Adicionando arquivos do php e site do jack

---
+++ PHP/Blog/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+    include './header.php'
+?>
+    <div class="col">
+        <h2>Bem vindo ao admin do Blog</h2>
+        <a href="./usuario/usuarioList.php">Usuario</a>
+        <a href="#">Postagem</a><br>
+        <a href="#">Categoria</a><br>
+    </div>
+
+<?php
+  include './footer.php'
+?>
